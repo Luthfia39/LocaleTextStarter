3af3fdab0b444557ca10acad97ff67f65fdab5c6
Luthfia39 <you@example.com>
2023-04-01 23:00:15 +0700

    Initial commit

--- app/src/main/java/com/example/localetextstarter/MainActivity.java
+++ app/src/main/java/com/example/localetextstarter/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.localetextstarter;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,15 +8,25 @@ import android.provider.Settings;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.text.DateFormat;
+import java.text.NumberFormat;
 import java.util.Date;
+import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView_price_100_packs;
+    private EditText input_price;
+    private double price;
+    private double price_100_packs;
+
+    private final static double USD_TO_IDR_EXCHANGE_RATE = 14968.65;
+    private final static double USD_TO_SAR_EXCHANGE_RATE = 3.75;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,17 +42,33 @@ public class MainActivity extends AppCompatActivity {
         });
 
 //    menentukan tgl kadaluarsa (misal 5 hari setelah hari ini)
-        Date myDate = new Date();
-        long expiredDate = myDate.getTime() + TimeUnit.DAYS.toMillis(5);
-        myDate.setTime(expiredDate);
+        expiredDate();
 
-        String formatDate = DateFormat.getDateInstance().format(myDate);
-        TextView expiredDateTexView = findViewById(R.id.date);
-        expiredDateTexView.setText(formatDate);
+        input_price = findViewById(R.id.edt_price);
+        textView_price_100_packs = findViewById(R.id.price100);
+
+        findViewById(R.id.btn_submit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                convertCurrency();
+            }
+        });
     }
 
+    private void convertCurrency() {
+        price = Double.parseDouble(input_price.getText().toString());
+        NumberFormat format = NumberFormat.getCurrencyInstance();
+        String toCurrency = Locale.getDefault().getCountry();
 
-
+        if (toCurrency == "ID"){
+            price_100_packs = price*100*USD_TO_IDR_EXCHANGE_RATE;
+        } else if (toCurrency == "EG") {
+            price_100_packs = price*100*USD_TO_SAR_EXCHANGE_RATE;
+        }else {
+            price_100_packs = price*100;
+        }
+        textView_price_100_packs.setText(format.format(price_100_packs));
+    }
 
     /**
      * Shows the Help screen.
@@ -88,5 +113,15 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void expiredDate(){
+        Date myDate = new Date();
+        long expiredDate = myDate.getTime() + TimeUnit.DAYS.toMillis(5);
+        myDate.setTime(expiredDate);
+
+        String formatDate = DateFormat.getDateInstance().format(myDate);
+        TextView expiredDateTexView = findViewById(R.id.date);
+        expiredDateTexView.setText(formatDate);
     }
 }
